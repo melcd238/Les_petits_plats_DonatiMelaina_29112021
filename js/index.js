6fb0f6a4338f0c5268b1976bc0b80f812e3a9c02
melcd238 <melcd238@gmail.com>
2021-12-08 12:36:52 +0100

    affichage des tags

--- js/index.js
+++ js/index.js
@@ -29,32 +29,43 @@ closeDropdown()
 // Filtrer par Tag
 let tagsArray = [];
 // Au click sur un li, on affiche le tag et on filtre la liste des recettes en fonction 
-// Ne pas afficher de doublons (si un tag est déjà affiché, on ne peut pas l'afficher deux fois)
-// utiliser data-filter dans li et tag (faire une fonction qui va remplacer les maj et caract spé à utiliser dans displaytList et dans le tag)
+
+// utiliser data-value dans li et tag 
 // recupérer tous les li plutôt que chaque li par liste
-const liIngredient = document.querySelectorAll('.liIngredient');
+const lis = document.querySelectorAll('.itemLi');
 const tagsContainer = document.querySelector('.tags');
 const listIngredient = document.querySelector('.list-ingredient-hide');
-liIngredient.forEach((li) => li.addEventListener('click',(e) =>{
-  tagsArray.push(li.textContent)
+const listAppareil =document.querySelector('.list-appareil-hide ');
+const listUstensil = document.querySelector('.list-ustensils-hide');
+lis.forEach((li) => li.addEventListener('click',(e) =>{
+   // condition pour ne pas afficher le doublon
+ if(!tagsArray.includes(e.target.dataset.value)){
+  tagsArray.push(e.target.dataset.value)
   console.log(tagsArray);
-  tagsContainer.innerHTML=`${tagsArray.map((tag)=>  
-    ` <div class="col-sm-auto btn-primary tag">${tag} <img src="/img/cross.svg" alt="croix de fermeture du tag" class="crossTag"> </div>`
-    ).join("")}`;
- // fermer la dropdown
- listIngredient.classList.add('hide');
- // filtrer les recettes 
-
- // Fermeture du tag et donc on filtre de nouveau l'affichage des recettes. 
-  const crossTags = document.querySelectorAll('.crossTag');
-  crossTags.forEach((cross) => cross.addEventListener('click', (e) =>{
-    e.preventDefault();
-    // On enlève le tag du tableau de tagsArray
-    // on modifie la liste des recettes affichée en fonction 
-   
-  }))
-
+     if(e.target.classList.contains('liIngredient')){
+       tagsContainer.innerHTML = `${tagsArray.map((tag)=> 
+        `<div class="col-sm-auto btn-primary tag" data-value='${tag}'>${tag} <img src="/img/cross.svg" alt="croix de fermeture du tag" class="crossTag"> </div>`
+        ).join("")}`;
+       // ferme la dropdown
+       listIngredient.classList.add('hide');
+     }
+     if(e.target.classList.contains('liAppliance')){
+      tagsContainer.innerHTML = `${tagsArray.map((tag)=> 
+        `<div class="col-sm-auto btn-success tag" data-value='${tag}'>${tag} <img src="/img/cross.svg" alt="croix de fermeture du tag" class="crossTag"> </div>`
+        ).join("")}`;
+        // ferme la dropdown
+        listAppareil.classList.add('hide');
+     }
+     if(e.target.classList.contains('liUstensil')){
+      tagsContainer.innerHTML = `${tagsArray.map((tag)=> 
+        `<div class="col-sm-auto btn-danger tag" data-value='${tag}'>${tag} <img src="/img/cross.svg" alt="croix de fermeture du tag" class="crossTag"> </div>`
+        ).join("")}`;
+        // ferme la dropdown
+        listUstensil.classList.add('hide');
+     }
+ }
 }))
+
 
 
 
