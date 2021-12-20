2abb0c4c88c5a2156095589d99cb1f8b2cf18cba
melcd238 <melcd238@gmail.com>
2021-12-20 14:07:16 +0100

    modif dropDown functions

--- js/utils/dropDown.js
+++ js/utils/dropDown.js
@@ -1,16 +1,20 @@
-
 const openDropdown = function (){
     const btnDropdown = document.querySelectorAll('.btn-dropdown');
     const listIngredient = document.querySelector('.list-ingredient-hide');
     const listAppareil =document.querySelector('.list-appareil-hide ');
     const listUstensil = document.querySelector('.list-ustensils-hide');
+    const btnAppareil = document.querySelector('.appareil');
+    const btnUstensil = document.querySelector('.ustensiles')
 
     btnDropdown.forEach((btn)=> btn.addEventListener('click', (e) =>{
         e.preventDefault();
         if(btn.classList.contains('btn-primary')) {
             listIngredient.classList.remove('hide');
+            btnAppareil.classList.add('move');
+            btnUstensil.classList.add('move');
         } else if(btn.classList.contains('btn-success')){
              listAppareil.classList.remove('hide');
+             btnUstensil.classList.add('move');
         } else if(btn.classList.contains('btn-danger')){
             listUstensil.classList.remove('hide');
          }
@@ -22,12 +26,16 @@ const closeDropdown = function (){
     const listAppareil =document.querySelector('.list-appareil-hide ');
     const listUstensil = document.querySelector('.list-ustensils-hide');
     const arrowUps = document.querySelectorAll('.arrowUp');
+    const btnAppareil = document.querySelector('.appareil');
+    const btnUstensil = document.querySelector('.ustensiles')
 
     arrowUps.forEach((arrowUp) => arrowUp.addEventListener('click', (e) => {
         e.preventDefault();
         listIngredient.classList.add('hide');
         listAppareil.classList.add('hide');
         listUstensil.classList.add('hide');
+        btnAppareil.classList.remove('move');
+        btnUstensil.classList.remove('move');
     }))
 }
 
